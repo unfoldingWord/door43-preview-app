a614a992df8cb475261a909407d733f26dddbd70
Richard Mahn <rich.mahn@unfoldingword.org>
2023-12-04 14:08:47 -0700

    Splits into resource components

--- src/components/AppWorkspace.jsx
+++ src/components/AppWorkspace.jsx
@@ -2,12 +2,8 @@ import { useState, useContext } from 'react'
 import Sheet from '@mui/joy/Sheet'
 import Card from '@mui/joy/Card'
 import Typography from '@mui/joy/Typography'
-import { useUsfmPreviewRenderer } from '@oce-editor-tools/base'
 import { PrintDrawer } from '@oce-editor-tools/joy-core'
-import DOMPurify from 'dompurify'
 import CircularProgressUI from '@mui/joy/CircularProgress'
-import {getLtrPreviewStyle, getRtlPreviewStyle} from "../lib/previewStyling.js"
-// import markdown from '../lib/drawdown'
 import { AppContext } from './App.context'
 import Header from './Header'
 import OpenModal from './OpenModal.jsx'
@@ -20,13 +16,23 @@ export default function AppWorkspace() {
 
   const {
     state: {
+      repo,
       catalogEntry,
+      resourceComponent,
       errorMessage,
-      loading, 
-      usfmText, 
-      usfmFileLoaded,
+      printHtml,
     },
   } = useContext(AppContext)
+
+  const printPreviewProps = {
+    openPrintDrawer: isOpenPrint && printHtml,
+    handleClosePrintDrawer: () => {
+      setIsOpenPrint(false)
+    },
+    onRenderContent: () => printHtml,
+    canChangeAtts: false,
+    canChangeColumns: true,
+  }
 
   const renderFlags = {
     showWordAtts: false,
@@ -41,76 +47,26 @@ export default function AppWorkspace() {
     showVersesLabels: true,
   }
 
-  const { 
-    renderedData,
-    ready: htmlReady 
-  } = useUsfmPreviewRenderer({ 
-    usfmText, 
-    renderFlags,
-    renderStyles: catalogEntry ? (catalogEntry.language_direction === 'rtl' ? getRtlPreviewStyle() : getLtrPreviewStyle()) : getLtrPreviewStyle(),
-    htmlRender: true
-  })
-
-  const usfmPreviewProps = {
-    openPrintDrawer: isOpenPrint && usfmFileLoaded && htmlReady,
-    handleClosePrintDrawer: () => {
-      setIsOpenPrint(false)
-    },
-    onRenderContent: () => renderedData,
-    canChangeAtts: false,
-    canChangeColumns: true,
-  }
-
-  // const mdPreviewProps = {
-  //   openPrintDrawer: isOpenPrint && mdFileLoaded && mdHtmlReady,
-  //   handleClosePrintDrawer: () => {
-  //     setIsOpenPrint(false)
-  //   },
-  //   onRenderContent: () => markdownHtmlStr,
-  //   canChangeAtts: false,
-  //   canChangeColumns: false,
-  // }
-
   return (
     <Sheet>
       <Header 
         title={APP_NAME}
         // infoLine={infoLine}
-        dcsRef={catalogEntry && `${BASE_DCS_URL}/${catalogEntry.owner}/${catalogEntry.repo.name}/src/${catalogEntry.ref_type}/${catalogEntry.branch_or_tag_name}`} 
-        ready={usfmFileLoaded}
+        dcsRef={repo && `${BASE_DCS_URL}/${repo.owner.username}/${repo.name}` + (catalogEntry ? `/src/${catalogEntry.ref_type}/${catalogEntry.branch_or_tag_name}` : '')} 
+        ready={printHtml != ""}
         onPrintClick={() => setIsOpenPrint(!isOpenPrint)}
         onOpenClick={() => setIsOpenModal(!isOpenModal)}
       />
       <Card sx={{marginTop: "70px"}}>
-        {/* { mdFileLoaded && <PrintModal {...mdPreviewProps} />}
-        { mdFileLoaded && (<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(markdownHtmlStr)}}/>)} */}
-        {usfmFileLoaded && <PrintDrawer {...usfmPreviewProps} />}
-        {usfmFileLoaded &&
-          (htmlReady ? (
-            <>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(renderedData),
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <Typography color="textPrimary" gutterBottom display="inline">
-                <>Converting from Usfm... </>
-              </Typography>
-              <CircularProgressUI />
-            </>
-          ))}
-        {loading && (
+        {printHtml && <PrintDrawer {...printPreviewProps} />}
+        {errorMessage ? <>{errorMessage}</> : resourceComponent ? resourceComponent :
           <>
             <Typography color="textPrimary" gutterBottom display="inline">
-              <>Loading file from server... </>
+              <>Loading from server... </>
             </Typography>
             <CircularProgressUI />
           </>
-        )}
-        {!usfmFileLoaded && <>{errorMessage}</>}
+        }
       </Card>
       <OpenModal 
         isOpenModal={isOpenModal}
