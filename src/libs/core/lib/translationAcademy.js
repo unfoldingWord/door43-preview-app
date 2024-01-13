983cd4b5e49e68093a3ea5c9a1556570b1a64564
Richard Mahn <rich.mahn@unfoldingword.org>
2024-01-13 05:08:19 -0700

    Other resources?

---
+++ src/libs/core/lib/translationAcademy.js
@@ -0,0 +1,27 @@
+import useFetchZipFileData from '@libs/core/hooks/useFetchZipFileData'
+
+export function usePopulateSupportReferences({catalogEntry, supportReferences}) {
+    const [populatedSupportReferences, setPopulatedSupportReferences] = useState({})
+
+    let zipFileData = null
+    try {
+      zipFileData = useFetchZipFileData({catalogEntry})
+    } catch (e) {
+      setErrorMessage(e.message)
+    }
+  
+    useEffect(() => {
+        const handlePopulatingSupportReferences = async () => {
+            const _populateSupportReferences = JSON.parse(JSON.stringify(supportReferences))
+            
+            Object.keys(_populateSupportReferences).forEach(rcRef => {
+                rcParts = rcRef.split("/")
+            
+            })
+        }
+
+        if(supportReferences && zipFileData) {
+            handlePopulatingSupportReferences()
+        }
+    }, [supportReferences, zipFileData])
+}
