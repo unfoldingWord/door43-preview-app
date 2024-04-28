d475611413b03c5098d5d9ea1bff6cfe6e091c82
Richard Mahn <rich.mahn@unfoldingword.org>
2024-04-28 03:45:24 -0600

    Adds a python script for testing

---
+++ hit_preview_pages.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+
+
+completed_urls = []
+try:
+  with open('./completed_urls', 'r') as file:
+    completed_urls = file.read().splitlines()
+except FileNotFoundError:
+  pass
+
+# Setup Chrome options
+chrome_options = webdriver.ChromeOptions()
+
+# Set the location of the webdriver
+webdriver_service = Service(ChromeDriverManager().install())
+
+# Initialize the driver
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+root = 'https://preview.door43.org'
+# Open the webpage
+driver.get(root + "?owner=unfoldingWord&owner=Door43-Catalog&subject=Aligned+Bible&subject=Bible&subject=Greek+New+Testament&subject=Hebrew+Old+Testament&subject=Open+Bible+Stories&subject=Translation+Words&subject=TSV+Translation+Notes&subject=TSV+Translation+Words+Links&subject=TSV+Translation+Questions&stage=prod&sort=released&order=desc")
+
+# Wait for the component to load
+wait = WebDriverWait(driver, 60)  # adjust the timeout as needed
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'css-1kv5bp7')))  # replace with the actual class name of your component
+
+print(driver.page_source)
+
+# Parse the page source with BeautifulSoup
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+print(str(soup))
+
+# Find all the URLs on the page
+urls = list(set([a['href'] for a in soup.find_all('a', href=True)]))
+
+# Loop through the URLs
+for url in urls:
+    # Open the page
+    print(url)
+
+    if not url.startsWith('/u/') or url in completed_urls:
+      continue
+
+    try:
+      print(root+url)
+      driver.get(root+url)
+
+      # Wait for the component to load
+      wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Error')] | //div[@id='web-preview'][child::*]")))
+
+      print("SUCCESS: "+url)
+      # Add a delay to ensure the page has fully loaded (adjust as needed)
+      # time.sleep(5)
+      # Read the completed URLs from the file if it exists
+
+      # Add the completed URL to the array
+      completed_urls.append(url)
+
+      # Save the completed URLs to the file
+      with open('./completed_urls', 'w') as file:
+        file.write('\n'.join(completed_urls))
+    except Exception as e:
+      print(e)
+      print("Error: "+url)
+      continue
+
+# Close the WebDriver
+driver.quit()
