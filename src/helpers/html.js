434d24fd459a36ea6d56aef6cdac8038951983ca
Richard Mahn <rich.mahn@unfoldingword.org>
2024-05-21 17:22:22 -0400

    Updates app info

--- src/helpers/html.js
+++ src/helpers/html.js
@@ -58,7 +58,7 @@ export function getDoor43PrevieAppVersionFooterHTML() {
   const formattedDate = date.toISOString().split('T')[0];
   return `
 <div id="app-version-info" style="font-size: 0.8em; display: flex; justify-content: space-between; border-top: 1px solid black; margin-top: 10px;">
-  <div><span style="font-weight: bold">Generated with:</span> <a href="${window.location.href}" target="_blank">Door43 Preview App</a></div>
+  <div><span style="font-weight: bold">Generated with:</span>&nbsp;<a href="${window.location.href}" target="_blank">Door43 Preview</a></div>
   <div><span style="font-weight: bold">Version:</span> ${APP_VERSION}</div>
   <div><span style="font-weight: bold">Date:</span> ${formattedDate}</div>
 </div>
