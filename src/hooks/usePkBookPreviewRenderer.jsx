16cb13de014901056b81c34fd390e3b91c6506e6
Richard Mahn <rich.mahn@unfoldingword.org>
2024-06-21 17:35:51 -0400

    Moves PK from OCE tools to this repo

---
+++ src/hooks/usePkBookPreviewRenderer.jsx
@@ -0,0 +1,114 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { Proskomma } from 'proskomma-core';
+import { SofriaRenderFromProskomma } from 'proskomma-json-tools';
+import sofria2WebActions from '../renderer/sofria2web';
+import { Render2React } from '../renderer/render2react';
+import { Render2Html } from '../renderer/render2html';
+import { getBcvVerifyStruct, isVerifiedWithBcvStruct } from '@helpers/bcvVerify';
+
+const defaultFlags = {
+  showWordAtts: false,
+  showTitles: true,
+  showHeadings: true,
+  showIntroductions: true,
+  showFootnotes: true,
+  showXrefs: true,
+  showParaStyles: true,
+  showCharacterMarkup: true,
+  showChapterLabels: true,
+  showVersesLabels: true,
+};
+
+export default function usePkBookPreviewRenderer(props) {
+  const {
+    pk, 
+    docId, 
+    bookId,
+    renderStyles,
+  } = props;
+
+  const [ready,setReady] = useState(false)
+
+  useEffect(() => {
+    if ((docId != null) && (pk != null) && (bookId != null)) {
+      setReady(true)
+    }
+  },[bookId, docId, pk])
+
+  const doRender = useCallback(({
+    renderFlags,
+    htmlRender,
+    verbose,
+    extInfo,
+  }) => {
+    const output = {}
+    if ((docId != null) && (pk != null) && (bookId != null)) {
+      const renderer = new SofriaRenderFromProskomma({
+        proskomma: pk,
+        actions: sofria2WebActions,
+      })
+
+      let renderers = null
+      if (htmlRender) { // create a class instance of Render2Html and then get render functions bound to instance
+        const newRenderer = new Render2Html(renderStyles)
+        renderers = newRenderer.getRenderers()
+      } else {
+        const newRenderer = new Render2React(renderStyles)
+        renderers = newRenderer.getRenderers()
+      }
+
+      const config = {
+        ...defaultFlags,
+        ...renderFlags,
+        bookId,
+        extInfo,
+        verifyBcv: getBcvVerifyStruct(extInfo),
+        doVerify: isVerifiedWithBcvStruct,
+        renderers,
+      }
+      try {
+        renderer.renderDocument({
+          docId,
+          config,
+          output,
+        })
+      } catch (err) {
+        if (verbose) console.log('Renderer', err)
+        throw err
+      }
+    }
+    return output.paras
+  },[bookId, docId, pk, renderStyles])
+
+  return {
+    ready,
+    doRender,
+  }
+}
+
+ /** Rendering flags *
+  renderFlags: PropTypes.objectOf(PropTypes.bool),
+  /** Extended info - to be displayed for some verses *
+  extInfo: PropTypes.any,
+  /** Whether to show extra info in the js console *
+  verbose: PropTypes.bool,
+  /** Whether to render in html - default is to render React  *
+  htmlRender: PropTypes.bool,
+*/
+
+usePkBookPreviewRenderer.defaultProps = {
+  renderStyles: null,
+}
+
+usePkBookPreviewRenderer.propTypes = {
+  /** Instance of Proskomma class */
+  pk: PropTypes.instanceOf(Proskomma),
+  /** docId - the id of this document - taken from Proskomma */
+  docId: PropTypes.string, 
+  /** bookId selector for what content to show in the preview */
+  bookId: PropTypes.string,
+  /** app preferred styles to use */
+  renderStyles: PropTypes.object,
+}
