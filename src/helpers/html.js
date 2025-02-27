bde771387d83fab6e8f7cdaa7a64684cdf1ec338
Richard Mahn <rich.mahn@unfoldingword.org>
2025-02-27 07:42:41 -0500

    Fixes converting paths to links

--- src/helpers/html.js
+++ src/helpers/html.js
@@ -39,7 +39,7 @@ export function convertNoteFromMD2HTML(note, bookId, chapterStr) {
   note = note.replace(/href="\.\.\/\.\.\/([1-3]*[a-z]+)\/0*([^/".]+)\/0*([^/".]+)(\.md){0,1}"/g, `href="${window.location.href.split('#')[0].split('?')[0]}?book=$1#$1-$2-$3" target="_blank"`);
   note = note.replace(/href="\.\.\/\.\.\/([1-3]*[a-z]+)\/0*([^/".]+)(\.md){0,1}"/g, `href="${window.location.href.split('#')[0].split('?')[0]}?book=$1#$1-$2" target="_blank"`);
   // handle a single version, href="../01.md" or "01.md" or "./10"
-  note = note.replace(/href="(\.+\/){0,1}0*([1-9]+)(\.md){0,1}"/g, `href="#nav-${bookId}-${chapterStr}-$1"`);
+  note = note.replace(/href="(\.+\/){0,1}0*([1-9]+)(\.md){0,1}"/g, `href="#nav-${bookId}-${chapterStr}-$2"`);
   note = transformRelativeUrls(note, bookId);
   note = note.replace(/(href="http[^"]+")/g, '$1 target="_blank"');
   note = note.replace(/<h4>/g, '<h6>').replace(/<\/h4>/g, '</h6>');
