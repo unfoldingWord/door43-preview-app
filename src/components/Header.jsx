2482f4e7a25cc783b13b0f010bafec7158695dd6
Richard Mahn <rich.mahn@unfoldingword.org>
2026-07-04 10:21:25 -0600

    Capitalization change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -489,7 +489,7 @@ export default function Header({ serverInfo, urlInfo, repo, owner, catalogEntry,
                 <div style={{ textAlign: 'center' }}>
                   <Tooltip title="View resource metadata on DCS" arrow>
                     <a href={catalogEntry.metadata_url} target="_blank" rel="noopener noreferrer">
-                      {"See resource's metadata"}
+                      {"See Resource's Metadata"}
                     </a>
                   </Tooltip>
                 </div>
